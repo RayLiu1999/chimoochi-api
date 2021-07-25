0dbaf22bb79324c34cf405b0560d61d99fc9b636
RayLiu1999 <rray12tw346@gmail.com>
2021-07-25 20:28:12 +0800

    make it better

--- app/Http/Middleware/CORSVerify.php
+++ app/Http/Middleware/CORSVerify.php
@@ -17,7 +17,7 @@ class CORSVerify
     public function handle(Request $request, Closure $next)
     {
         $origin = $request->header('Origin');
-        $domain = ['http://127.0.0.1:8000', 'http://localhost:8000','https://6yuwei.com', 'https://chimoochi-api.herokuapp.com/'];
+        $domain = ['http://127.0.0.1:8000', 'http://localhost:8080','https://6yuwei.com', 'https://chimoochi-api.herokuapp.com'];
         $methods = 'PUT, GET, POST, DELETE, OPTIONS';
 
         if ($origin && collect($domain)->contains($origin)) {
