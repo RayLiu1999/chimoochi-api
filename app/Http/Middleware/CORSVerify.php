8529c9a437a5db7f06a22a74cf6b0a294064da1f
RayLiu1999 <rray12tw346@gmail.com>
2021-07-25 22:13:34 +0800

    make it better

--- app/Http/Middleware/CORSVerify.php
+++ app/Http/Middleware/CORSVerify.php
@@ -34,6 +34,7 @@ class CORSVerify
         return $next($request)
                     ->header('Access-Control-Allow-Origin', '*')
                     ->header('Access-Control-Allow-Methods', '*')
-                    ->header('Access-Control-Allow-Headers', '*');
+                    ->header('Access-Control-Allow-Headers', '*')
+                    ->header('Access-Control-Allow-Credentials', true);
     }
 }
