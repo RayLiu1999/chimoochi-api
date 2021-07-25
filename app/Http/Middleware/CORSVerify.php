0a370249a51b8be4404df3d65a532ba0a1a72bda
RayLiu1999 <rray12tw346@gmail.com>
2021-07-25 20:22:26 +0800

    make it better

--- app/Http/Middleware/CORSVerify.php
+++ app/Http/Middleware/CORSVerify.php
@@ -17,16 +17,16 @@ class CORSVerify
     public function handle(Request $request, Closure $next)
     {
         $origin = $request->header('Origin');
-        $domain = ['http://127.0.0.1:8000', 'https://6yuwei.com', 'https://chimoochi-api.herokuapp.com/'];
+        $domain = ['http://127.0.0.1:8000', 'http://localhost:8000','https://6yuwei.com', 'https://chimoochi-api.herokuapp.com/'];
         $methods = 'PUT, GET, POST, DELETE, OPTIONS';
-        
+
         if ($origin && collect($domain)->contains($origin)) {
             return $next($request)
-                    ->header('Access-Control-Allow', $origin)
+                    ->header('Access-Control-Allow-Origin', $origin)
                     ->header('Access-Control-Allow-Methods', $methods)
                     ->header('Access-Control-Allow-Credentials', true);
         } else {
-            return response()->json(['error' => 'CORS error'], 404)->header('Access-Control-Allow', $domain[0]);
+            return response()->json(['error' => 'CORS error'], 404)->header('Access-Control-Allow-Origin', $domain[0]);
         }
 
         // return $next($request)
