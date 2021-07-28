6ac06741c51ce6c311863d1625950277a3a36dab
RayLiu1999 <rray12tw346@gmail.com>
2021-07-28 14:46:31 +0800

    make it better

--- app/Http/Middleware/CORSVerify.php
+++ app/Http/Middleware/CORSVerify.php
@@ -22,10 +22,10 @@ class CORSVerify
         
         if ($origin && collect($domain)->contains($origin)) {
             return $next($request)
-                    ->header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500')
+                    ->header('Access-Control-Allow-Origin', $origin)
                     ->header('Access-Control-Allow-Methods', $methods)
                     ->header('Access-Control-Allow-Credentials', true)
-                    ->header('Access-Control-Allow-Headers', '*');
+                    ->header('Access-Control-Allow-Headers', 'Origin, Methods, Content-Type');
         } else {
             return response()->json(['error' => 'CORS error'], 404);
         }
