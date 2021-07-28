8ef8b95cdb9cac2b4ae5c079bcdbf71bd026537b
RayLiu1999 <rray12tw346@gmail.com>
2021-07-28 10:00:37 +0800

    make it better

--- app/Http/Middleware/CORSVerify.php
+++ app/Http/Middleware/CORSVerify.php
@@ -19,22 +19,23 @@ class CORSVerify
         $origin = $request->header('Origin');
         $domain = ['http://127.0.0.1:8000', 'http://localhost:8080', 'https://6yuwei.com', 'https://chimoochi-api.herokuapp.com', 'http://127.0.0.1:8080', 'http://127.0.0.1:5500'];
         $methods = 'PUT, GET, POST, DELETE, PATCH, OPTIONS';
+        
 
-        // if ($origin && collect($domain)->contains($origin)) {
-        //     return $next($request)
-        //             ->header('Access-Control-Allow-Origin', $origin)
-        //             ->header('Access-Control-Allow-Methods', $methods)
-        //             ->header('Access-Control-Allow-Credentials', true)
-        //             ->header('Access-Control-Allow-Headers', '*');
-        // } else {
-        //     return response()->json(['error' => 'CORS error'], 404)->header('Access-Control-Allow-Origin', $domain[2]);
-        // }
+        if ($origin && collect($domain)->contains($origin)) {
+            return $next($request)
+                    ->header('Access-Control-Allow-Origin', $origin)
+                    ->header('Access-Control-Allow-Methods', $methods)
+                    ->header('Access-Control-Allow-Credentials', true)
+                    ->header('Access-Control-Allow-Headers', '*');
+        } else {
+            return response()->json(['error' => 'CORS error'], 404);
+        }
 
 
-        return $next($request)
-                    ->header('Access-Control-Allow-Origin', '*')
-                    ->header('Access-Control-Allow-Methods', '*')
-                    ->header('Access-Control-Allow-Headers', '*')
-                    ->header('Access-Control-Allow-Credentials', true);
+        // return $next($request)
+        //             ->header('Access-Control-Allow-Origin', '*')
+        //             ->header('Access-Control-Allow-Methods', '*')
+        //             ->header('Access-Control-Allow-Headers', '*')
+        //             ->header('Access-Control-Allow-Credentials', true);
     }
 }
